dff78d8fad87b03427a096a7715fb422133c0c13
N3ckl3ss <WhyTh3Fuck98>
2021-03-18 11:09:41 +0100

    Bootstrap imported

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Slider>
+    <Accordion>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Slider from './components/Slider.vue'
+import Accordion from './components/Accordion.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Slider,
+    Accordion
   }
 }
 </script>
